838fec827e82c73794e0bfea5336bee6acb30280
Prethush <ktmirash49@.gmail.com>
2021-08-02 13:35:16 +0530

    token verification

---
+++ block-BNaafs/authentication/middlewares/auth.js
@@ -0,0 +1,21 @@
+let jwt = require('jsonwebtoken');
+
+require("dotenv").config();
+
+module.exports = {
+    varifyToken: async (req, res, next) => {
+        let token = req.headers.authorization;
+        try {
+            if(token) {
+                let payload = await jwt.verify(token, process.env.SECRET);
+                req.user = payload;
+                return next();
+            } else {
+                return res.status(400).json({error: "Token Required"});
+            }
+        } catch(error) {
+            next(error);
+        }
+        
+    }
+}
